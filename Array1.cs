c5b51b108bb5ab8b601fffbd92f1e3c91993b313
Sant090 <120807532+Sant090@users.noreply.github.com>
2024-05-05 19:01:46 -0500

    Create Array1.cs

---
+++ Array1.cs
@@ -0,0 +1,18 @@
+string[] estudiantes = new string[] { "Sergio", "Santiago", "Motoa" };
+string[] materias = new string[] { "Calculo Diferencial" };
+int[] promedio = new int[estudiantes.Length];
+int[] notas = new int[5];
+
+for (int i = 0; i < estudiantes.Length; i++)
+{
+    Console.WriteLine($"{estudiantes[i]} por favor ingresar su calificacion para {materias[0]}");
+    for (int j = 0; j < notas.Length; j++)
+    {
+        notas[j] = Convert.ToInt16(Console.ReadLine());
+        promedio[i] += notas[j];
+    }
+}
+for (int i = 0; i < estudiantes.Length; i++)
+{
+    Console.WriteLine($"{estudiantes[i]} su promedio es {promedio[i] / 5}");
+}
